8fc2b2d2adf21701f738023ddc947350e4fa3f4c
Jang Haemin <io@jhaemin.com>
2020-03-11 11:46:33 +0900

    Two Sum

---
+++ LeetCode/[Easy] Two Sum/sol.0.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  for (let i = 0; i < nums.length; i += 1) {
+    for (let j = i + 1; j < nums.length; j += 1) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
+}
---
+++ LeetCode/[Easy] Two Sum/sol.jakesully.js
@@ -0,0 +1,9 @@
+const twoSum = function(nums, target) {
+  const comp = {}
+  for (let i = 0; i < nums.length; i++) {
+    if (comp[nums[i]] >= 0) {
+      return [comp[nums[i]], i]
+    }
+    comp[target - nums[i]] = i
+  }
+}
