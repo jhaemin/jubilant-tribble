927ade58a6f00c6b2fbf8f0e21978f6aca898f86
jhaemin <io@jhaemin.com>
2020-05-06 16:54:27 +0900

    Longest Palindromic Substring

---
+++ LeetCode/5. [Medium] Longest Palindromic Substring/sol.dynamic-programming.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+/*
+  dp[i][j] = s[i] === s[j] ? dp[i + 1][j - 1] : false;
+*/
+var longestPalindrome = function (s) {
+  const m = s.length
+  const dp = [...new Array(m)].map(() => new Array(m).fill(false))
+
+  let max = ''
+
+  for (let length = 1; length <= m; length++) {
+    for (let i = 0; i < m - length + 1; i++) {
+      const j = i + length - 1
+
+      dp[i][j] = s[i] === s[j] && (length <= 2 || dp[i + 1][j - 1])
+
+      // If dp[i][j] is `true` and it's length is longer than the previous max substring, update the max
+      if (dp[i][j] && length > max.length) {
+        max = s.substring(i, i + length)
+      }
+    }
+  }
+
+  return max
+}
